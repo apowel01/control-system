91f5340f78f7b913f6d4738f5bc6d24d43aaa4df
CertainlyCanyon <Canyonman133@gmail.com>
2018-11-08 19:07:36 -0800

    motor voltage and current placeholders.

--- HealthMCU/HealthMCU.ino
+++ HealthMCU/HealthMCU.ino
@@ -10,6 +10,8 @@
 
 #include <SoftwareSerial.h>
 
+
+
 /*
  * Main loop
  */
@@ -22,4 +24,18 @@ void setup() {
   Serial.begin(9600); // start serial for output
 
   Serial.println("Ready!");
+}
+
+/*
+ * returns the motor voltage of the input motor
+ */
+double get_motor_voltage(int motor_num) {
+  return 999999999;
+}
+
+/*
+ * returns the motor current of the input motor
+ */
+double get_motor_current(int motor_num) {
+  return 999999999;
 }
