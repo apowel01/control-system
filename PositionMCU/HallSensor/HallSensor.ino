7d3bea84856b6dfa28569156e42b763b6fa5e1c6
Alamsi <Alaa.alamsi@gmail.com>
2019-04-27 16:51:27 -0700

    Hall Sensor (Counting RPMs)
    The code counts RPMs now

--- PositionMCU/HallSensor/HallSensor.ino
+++ PositionMCU/HallSensor/HallSensor.ino
@@ -1,42 +1,54 @@
-const int pin3 = 3;
+//Decoder documentations
+//http://www.ti.com/lit/ds/symlink/sn74ls90.pdf
+
+
+//The R pins on the board
 const int pin22 = 22;
 const int pin24 = 24;
 const int pin26 = 26;
 const int pin28 = 26;
-const int interruptPin = 2;
 
-volatile int counter = 0;
+//Change frequency by moving pin 2 to QA, QB, QC or QD ranked from smallest to largest
+const int hallSensorOutput = 2;
+
+unsigned volatile int counter = 0;
 float revolutions;
 
-
+unsigned long currentMillis;
+unsigned long lastMillis;
+int RPM;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   Serial.begin(9600);
-  pinMode(pin3, OUTPUT);
   pinMode(pin22, OUTPUT);
   pinMode(pin24, OUTPUT);
   pinMode(pin26, OUTPUT);
   pinMode(pin28, OUTPUT);
 
+  //Set the R pins to LOW to start counting
   digitalWrite(pin22, LOW);
   digitalWrite(pin24, LOW);
   digitalWrite(pin26, LOW);
   digitalWrite(pin28, LOW);
   
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), tick, CHANGE);
+  pinMode(hallSensorOutput, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(hallSensorOutput), tick, CHANGE);
 }
 
 
 void loop() {
-  digitalWrite(pin3, HIGH);
-//  Serial.println(digitalRead(interruptPin2));
-  delay(60);
-  digitalWrite(pin3, LOW);
-  delay(60);
-  revolutions = counter/14;
-  Serial.println(revolutions);
+  currentMillis = millis();
+  revolutions = counter/14.0;
+  
+  if( (currentMillis-lastMillis)>100){
+    counter = 0;
+    RPM = revolutions*10*60;
+    lastMillis = currentMillis;
+  }
+  Serial.print(RPM);
+  Serial.println(" RPMs");
+
 }
 
 void tick() {
