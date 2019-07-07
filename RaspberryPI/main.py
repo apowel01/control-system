9a969b58436a7098dc6ce953b835944d9aa8ec24
Michael Johnston <MichaelTJohnston@Gmail.com>
2019-07-07 14:18:25 -0700

    - Added initial GUI telemetry server - main2.py - Updated to use correct "and" and "or" Python syntax

--- RaspberryPI/main.py
+++ RaspberryPI/main.py
@@ -2,25 +2,31 @@
 #### The FSM for the Raspberry Pi Control System ####
 
 # Imports
+import json
 import asyncio # Used for asynchronous functions
 import socket # Used for network communications
 from FSMClasses import FSM # Grabs the FSM object
 
 # Command server
 async def cmd_server(host, port):
-	server = await asyncio.start_server(processNetwork, host, port)
+	server = await asyncio.start_server(processNetCmds, host, port)
+	await server.serve_forever()
+
+# Telemetry server
+async def tlm_server(host, port):
+	server = await asyncio.start_server(processTlm, host, port)
 	await server.serve_forever()
 
 # Process network commands
-async def processNetwork(reader, writer):
+async def processNetCmds(reader, writer):
+	global pod # Grab global pod object
+
 	# List of possible commands
-	global pod
 	cmds = {'state': {'set': '', 'current': ''}}
 
 	while True:
-		# writer.write("Hi".encode('utf8'))
 		data = await reader.read(100) # Max number of bytes to read
-		if not data: # If data doesn't work, ignore
+		if not data: # If no commands, stop
 			break
 
 		# Split command as a list
@@ -50,6 +56,29 @@ async def processNetwork(reader, writer):
 
 		output = output + "\n" # Add a newline character to output for conveinience
 		writer.write(output.encode('utf8'))
+
+	writer.close()
+
+# Process telemetry for telemetry server
+async def processTlm(reader, writer):
+	global pod # Grab global pod object
+	# Init telemetry dictionary
+	telemetry = {'state': str(pod.state), 'distance': 0}
+
+	# Primary process loop
+	while True:
+		if writer.is_closing():
+			# Watch for the client to close
+			break
+		
+		telemetry['state'] = str(pod.state)
+		telemetry['distance'] = telemetry['distance'] + 1 # Sample data
+
+		output = json.dumps(telemetry) # Serialize dictionary as json
+		writer.write(output.encode('utf8'))
+
+		await asyncio.sleep(.1) # Wait until sending telem update
+
 	writer.close()
 
 # Instantiate the FSM as pod & drop into Startup
@@ -58,18 +87,17 @@ async def init_pod():
 	pod = FSM()
 
 async def main():
+	IP = "192.168.0.6" # socket.gethostbyname(socket.gethostname()) # Grab Pi's IP address
+
+	print(f"Host name: {socket.gethostname()}")
+	print(f"IP address: {IP}")
+
 	# Gather asynchronous coroutines
 	await asyncio.gather(
-		cmd_server('127.0.0.1', 5000),
+		cmd_server(IP, 5000),
+		tlm_server(IP, 5001),
 		init_pod()
 	)
 
 asyncio.run(main())
 
-# pod.trigger('ready_to_launch')
-# pod.trigger('launching')
-# pod.trigger('coasting')
-# pod.trigger('braking')
-# pod.trigger('crawling')
-# pod.trigger('braking')
-# pod.trigger('safe_to_approach')
