cd5f6c8343665f09e6e5c2a97e610af6313841a0
Alamsi <alaa.alamsi@example.com>
2019-07-14 00:39:50 -0700

    more error proffing on position.py

--- RaspberryPI/position.py
+++ RaspberryPI/position.py
@@ -19,6 +19,14 @@ accelerations = [] #2D list containing [acceleration, time stamp]
 #TotalDistance
 CurrentDistance = 0
 
+#Used to error proof lidar
+#Allow up to 5 errors, then completely ignore the lidar
+#Errors are counted when acceleration spikes unnaturally due to lidar
+lidarErrorCounter = 0
+
+#Indicates wheather or not position is determined by lidar for error countering reasons
+lidarInUse = False
+
 #LOOP FROM HERE
 circumfrence_of_wheels = #####
 lidarReading = #Get from Arduino
@@ -43,13 +51,15 @@ LaserSensor_1 = Number_of_bands_1*3048 #100ft = 3048cm
 LaserSensor_2 = Number_of_bands_2*3048 #100ft = 3048cm
 
 '''PREPARE LIDAR DATA DATA'''
-if Lidar < 10000 and CurrentDistance > 100000: #If lidar within last 100 meters and has crossed 1000 meters
+#If lidar within last 100 meters and the pod has crossed 1000 meters and less than 5 errors from lidar occured
+if Lidar < 10000 and CurrentDistance > 100000 and lidarErrorCounter <5:
     CurrentDistance = 125000-Lidar 
+    lidarInUse = True
 else:
     '''PREPARE LASERSENSOR DATA'''
-    if LaserSensor_1/LaserSensor_2 > 1.10:
+    if LaserSensor_2 != 0 and LaserSensor_1/LaserSensor_2 > 1.10:
         LaserSensor_distance =  LaserSensor_1 #Take sensor_1 if its higher and difference > 10%
-    elif LaserSensor_2/LaserSensor_1 > 1.10:
+    elif LaserSensor_1 != 0 and LaserSensor_2/LaserSensor_1 > 1.10:
         LaserSensor_distance =  LaserSensor_2 #Take sensor_2 if its higher and difference > 10%
     else:
         LaserSensor_distance = (LaserSensor_1+LaserSensor_2)/2 #Take average if two sensors are close within each other
@@ -72,25 +82,43 @@ else:
     HallSensor_distance = HallSensor_distance/len(HallSensors_withinRange)
 
     '''DECIDE WHEATHER TO GO WITH HALLSENSOR OR LASERSENSOR '''
-    if (HallSensor_distance/LaserSensor_distance-1) < .1: #Take the Hall Sensor data as distance ONLY when its within a 10% range and greater than the laser sensor readings
+    if LaserSensor_distance == 0: #if laser sensor reading is zero
+        if (time.time() - startTime) > 2: #Wait until we have position data history
+            previousPosition = positions[-1][0]
+            previousVelocity = velocities[-1][0]
+            estimatedCurrentPosition = previousPosition+previousVelocity*t(ime.time()-positions[-1][1]) #previousPosition+previousVelocity*time
+            currentPosition = HallSensor_distance+previousPosition
+            if currentPosition/estimatedCurrentPosition < 1.05: #if estimated position and position from hall sensor are within 5% of each other, go with hallsensor
+                CurrentDistance += HallSensor_distance
+    elif (HallSensor_distance/LaserSensor_distance-1) < .1: #Take the Hall Sensor data as distance ONLY when its within a 10% range and greater than the laser sensor readings
         CurrentDistance += HallSensor_distance
     else:
         CurrentDistance += LaserSensor_distance
 
+position = CurrentDistance
 
 #Get the current time since start
 currentTime = time.time() - startTime
+startTime = time.time()
+
+positions.append([position, currentTime])
 
 #calculate velocity (CM/Sec)
-positions.append([CurrentDistance, currentTime])
-for pos, time in positions:
-    velocity = float(pos/time)
-    if velocity > 8940: #If velocity recorded is over 8940cm/s (200mph). There is an error
-        velocities.append([velocity, time])
-    else:
-        velocities.append([velocity, time])
+velocity = float(position/currentTime)
+velocities.append([velocity, currentTime])
 
 #calculate acceleration
-for vel, time in velocities:
-    acceleration = float(vel/time)
-    accelerations.append([acceleration, time])
+acceleration = float(velocity/currentTime)
+if acceleration > 200: #If acceleration recorded is over 200cm/s/s. There is an error
+    if lidarInUse:
+        lidarErrorCounter += 1
+    acceleration = accelerations[-1][0] #update acceleration with last acceleration value
+    velocities[-1][0] = velocities[-2][0] #update velocity with last velocity value
+    velocity = velocities[-1][0]
+    positions[-1][0] = positions[-2][0]+velocity*(positions[-1][1]-positions[-2][1])  #update positions with: lastPosition+lastVelocity*timeInterval
+    position = positions[-1][0]
+accelerations.append([acceleration, currentTime])
+
+#position
+#velocity
+#acceleration
