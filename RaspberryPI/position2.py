b304e6fb89aa88b5f10593f74c3d24d4ddec536b
Alamsi <alaa.alamsi@example.com>
2019-07-17 11:58:26 -0700

    debugged position code

--- RaspberryPI/position2.py
+++ RaspberryPI/position2.py
@@ -1,6 +1,8 @@
 import asyncio
 import time
 
+import threading
+
 def median(lst):
     n = len(lst)
     s = sorted(lst)
@@ -25,7 +27,8 @@ hallSensor_total = 0
 lastBand_1 = 0
 lastBand_2 = 0
 
-async def updatePosition(lidarReading, rpms_list, bands_list):
+# async def updatePosition(lidarReading, rpms_list, bands_list):
+def updatePosition(lidarReading, rpms_list, bands_list):
     global startTime
 
     global positions
@@ -40,26 +43,28 @@ async def updatePosition(lidarReading, rpms_list, bands_list):
 
     #Get the current time since start
     currentTime = time.time() - startTime
-    currentTimestep = time.time()-currentTime
+    currentTimestep = time.time()-positions[-1][1]-startTime
+    # currentTimestep = time.time()-currentTime 
 
     #TotalDistance (gets last recorded position)
     CurrentDistance = positions[-1][0]
+    print("current distance, ", CurrentDistance)
 
     #Used to calculate distance from revolutions
-    circumfrence_of_wheels = #####
+    circumfrence_of_wheels = 3048#####
 
     #Get readings from arduinos
     #convert readings to cm
     Lidar = (1250 - lidarReading)*100
-
-    HallSensors = [rpm*circumfrence_of_wheels*currentTimestep for rpm in rpms_list]
+    print(currentTimestep)
+    HallSensors = [rpm*circumfrence_of_wheels*currentTimestep*(1/60) for rpm in rpms_list]
 
     LaserSensor_1 = bands_list[0]*3048 #100ft = 3048cm
     LaserSensor_2 = bands_list[1]*3048 #100ft = 3048cm
 
     '''PREPARE LIDAR DATA DATA'''
     #If lidar within last 100 meters and the pod has crossed 1000 meters and less than 5 errors from lidar occured
-    if Lidar < 10000 and CurrentDistance > 100000 and lidarErrorCounter <5:
+    if lidarReading < 10000 and CurrentDistance > 100000 and lidarErrorCounter <5:
         CurrentDistance = Lidar 
     else:
         '''PREPARE HALLSENSOR DATA'''
@@ -74,25 +79,23 @@ async def updatePosition(lidarReading, rpms_list, bands_list):
             HallSensor_distance += HallSensor
         HallSensor_distance = HallSensor_distance/len(HallSensors_withinRange)
 
-        if LaserSensor_1 != lastBand_1 or LaserSensor_2 != lastBand_2:
-            hallSensor_total += HallSensor_distance #Keeps track of how long we have traveled with Hall Sensor
-        else:
-            hallSensor_total = 0 #Clear hall sensor distance if we just detected a band
-
-
         '''PREPARE LASERSENSOR DATA''' #GET BACK TO THIS
-        if LaserSensor_1 > LaserSensor_2:
-            LaserSensor_distance =  LaserSensor_1
-            lastBand_1 = LaserSensor_1
-            hallSensor_total = 0 #Reset hall sensor distance to 0 when a band is sensed
+        if LaserSensor_1 != lastBand_1 or LaserSensor_2 != lastBand_2:
+            hallSensor_total = 0  #Clear hall sensor distance if we just detected a band
+            if LaserSensor_1 > LaserSensor_2:
+                LaserSensor_distance =  LaserSensor_1
+                lastBand_1 = LaserSensor_1
+            else:
+                LaserSensor_distance =  LaserSensor_2
+                lastBand_2 = LaserSensor_2
         else:
-            LaserSensor_distance =  LaserSensor_2
-            LaserSensor_2 = LaserSensor_2
-            hallSensor_total = 0 #Reset hall sensor distance to 0 when a band is sensed
+            if HallSensor_distance >= 3048: #if hallsensor distance calculated is over 100ft, then block it at 100ft
+                hallSensor_total = 3048
+            else:
+                hallSensor_total += HallSensor_distance #Keeps track of how long we have traveled with Hall Sensor
 
-
-
-    position = LaserSensor_distance + hallSensor_total
+    CurrentDistance = LaserSensor_distance + hallSensor_total
+    position = CurrentDistance
 
     #Insert position
     positions.append([position, currentTime])
@@ -106,9 +109,53 @@ async def updatePosition(lidarReading, rpms_list, bands_list):
     acceleration = float(velocity/currentTime)
     accelerations.append([acceleration, currentTime])
 
-    if Lidar < 10000 and CurrentDistance < 100000 and lidarErrorCounter < 5:
+    #Check if an error occured with the lidar
+    #Allow up to 5 errors
+    if lidarReading < 10000 and CurrentDistance < 100000 and lidarErrorCounter < 5:
         lidarErrorCounter += 1
 
     #Returns current data
     #history of data can be accessed via the global lists (positions, velocities, accelerations)
     return [position, velocity, acceleration]
+
+
+
+
+
+'''SIMULATION'''
+
+rpmCount_1 = 0#Get from Arduino
+rpmCount_2 = 0#Get from Arduino
+rpmCount_3 = 0#Get from Arduino
+rpmCount_4 = 0#Get from Arduino
+bandsTotal_1=0
+bandsTotal_2=0
+def main():
+    threading.Timer(1, main).start()
+
+    global rpmCount_1
+    global rpmCount_2
+    global rpmCount_3
+    global rpmCount_4
+    global bandsTotal_1
+    global bandsTotal_2
+
+
+    lidarReading = 90
+
+    rpmCount_1 = 60
+    rpmCount_2 = 60
+    rpmCount_3 = 60
+    rpmCount_4 = 60
+    revoloutions_list = [rpmCount_1,rpmCount_2,rpmCount_3,rpmCount_4]
+    
+    bandsTotal_1 += 1
+    bandsTotal_2 += 1
+
+    bands_list = [bandsTotal_1, bandsTotal_2]
+
+    currentData = updatePosition(lidarReading, revoloutions_list, bands_list)
+    print(currentData)
+    # await asyncio.wait(currentData)
+
+main()
